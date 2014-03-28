ec0389f0e0a2c47869d5b1217d21b2c71d9cd8f8
Veronica <veronica.milcheva@telerik.com>
2014-03-28 18:28:44 +0200

    Committed Unit test project

---
+++ TestimonialsUnitTests/IAmAUnitTestSuite.cs
@@ -0,0 +1,27 @@
+﻿using MbUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestimonialsUnitTests
+{
+    [TestFixture]
+    public class IAmAUnitTestSuite
+    {
+        [Test]
+        [Author("Nyan Cat")]
+        public static void IAmAPassingUnitTest()
+        {
+            Assert.IsFalse(1 == 2);
+        }
+
+        [Test]
+        [Author("Doge")]
+        public static void IAmAFailingUnitTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
