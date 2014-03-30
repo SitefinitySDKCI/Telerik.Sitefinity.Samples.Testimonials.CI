a2d155999ec03d589b2822ca4c60547ac72e19e9
nader-dab <nader.dabour@abv.bg>
2014-03-30 17:53:35 +0300

    Added 3 passing unit tests

--- TestimonialsUnitTests/IAmAUnitTestSuite.cs
+++ TestimonialsUnitTests/IAmAUnitTestSuite.cs
@@ -10,12 +10,12 @@ namespace TestimonialsUnitTests
     [TestFixture]
     public class IAmAUnitTestSuite
     {
-        //[Test]
-        //[Author("Nyan Cat")]
-        //public static void IAmAPassingUnitTest()
-        //{
-        //    Assert.IsFalse(1 == 2);
-        //}
+        [Test]
+        [Author("Nyan Cat")]
+        public static void IAmAPassingUnitTest()
+        {
+            Assert.IsFalse(1 == 2);
+        }
 
         [Test]
         [Author("Doge")]
@@ -24,19 +24,18 @@ namespace TestimonialsUnitTests
             Assert.IsTrue(1 == 1);
         }
 
-        [Test]
-        [Author("Doge")]
-        public static void IAmAFailingUnitTest()
-        {
-            Assert.IsTrue(1 == 2);
-        }
-
         //[Test]
         //[Author("Doge")]
-        //public static void IAmAFailingUnitTestThatWork()
+        //public static void IAmAFailingUnitTest()
         //{
-        //    Assert.IsTrue(1 == 1);
+        //    Assert.IsTrue(1 == 2);
         //}
 
+        [Test]
+        [Author("Doge")]
+        public static void IAmAFailingUnitTestThatWork()
+        {
+            Assert.IsTrue(1 == 1);
+        }
     }
 }
